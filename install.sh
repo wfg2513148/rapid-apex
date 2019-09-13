8af0d579fb40c3d853d47c8eae1d7cf5999c2df6
wfg2513148 <wfgdlut@gmail.com>
2019-09-13 17:47:19 +0800

    $*

--- install.sh
+++ install.sh
@@ -56,27 +56,7 @@ fi;
 
 
 echo ">>> print all input parameters..."
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-echo $6
-echo $7
-echo $8
-echo $9
-echo $10
-echo $11
-echo $12
-echo $13
-echo $14
-echo $15
-echo $16
-echo $17
-echo $18
-echo $19
-echo $20
-echo $21
+echo $*
 echo ">>> end of print all input parameters..."
 exit;
 
