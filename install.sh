e24714cf6322b3a37dbf62b81d72d5d68053cb3c
wfg2513148 <wfgdlut@gmail.com>
2019-09-13 17:33:14 +0800

    add debug in install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,6 @@ ords_port=32513 # $19
 ords_file_name=ords-19.2.0.199.1647.zip # $20
 ords_version=19.2.0 # $21
 
-echo ">>> $1"
 
 if [ $1="N" ]; then
   use_exist_media=$2
@@ -51,32 +50,35 @@ if [ $1="N" ]; then
   ords_file_name=$20
   ords_version=$21
   echo ">>> you choose custom install mode..."
-  echo $1
-  echo $2
-  echo $3
-  echo $4
-  echo $5
-  echo $6
-  echo $7
-  echo $8
-  echo $9
-  echo $10
-  echo $11
-  echo $12
-  echo $13
-  echo $14
-  echo $15
-  echo $16
-  echo $17
-  echo $18
-  echo $19
-  echo $20
-  echo $21
-  exit;
 else
   echo ">>> you choose quick install mode..."
-  exit;
 fi;
+
+
+echo ">>> print all input parameters..."
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo $7
+echo $8
+echo $9
+echo $10
+echo $11
+echo $12
+echo $13
+echo $14
+echo $15
+echo $16
+echo $17
+echo $18
+echo $19
+echo $20
+echo $21
+echo ">>> end of print all input parameters..."
+exit;
 
 ##############################################################################################################
 
