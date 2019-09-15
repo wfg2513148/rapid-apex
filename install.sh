443ba7be3757c63cdcb411f0c71f94a891e4ab76
wfg2513148 <wfgdlut@gmail.com>
2019-09-15 09:03:50 +0800

    add pull logic in install.sh

--- install.sh
+++ install.sh
@@ -26,6 +26,8 @@ ords_pass=oracle # $18
 ords_port=32513 # $19
 ords_file_name=ords-19.2.0.199.1647.zip # $20
 ords_version=19.2.0 # $21
+aliyun_docker_account='' # $22
+aliyun_docker_password='' # $23
 
 
 quick_install=$1
@@ -51,6 +53,8 @@ if [ "$quick_install" = "N" ]; then
   ords_port=${19}
   ords_file_name=${20}
   ords_version=${21}
+  aliyun_docker_account=${22}
+  aliyun_docker_password=${23}
   echo ">>> you choose custom install mode..."
 else
   echo ">>> you choose quick install mode..."
@@ -138,7 +142,8 @@ if [[ "$(docker images -q registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-xe:
   docker build -t registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-xe:$db_version --build-arg DB_SYS_PWD=$db_sys_pwd .
   echo ">>> docker image registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-xe:$db_version does not exist, begin to build docker image..."
 else
-  echo ">>> docker image registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-xe:$db_version exists, skip compile and reuse it..."
+  echo ">>> docker image registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-xe:$db_version exists, pull from aliyun docker repository..."
+  docker pull registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-xe:$db_version
 fi
 
 
@@ -186,7 +191,8 @@ if [[ "$(docker images -q registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-ord
   echo ">>> docker image registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-ords:$ords_version does not exist, begin to build docker image..."
   docker build -t oracle-ords:$ords_version .
 else
-  echo ">>> docker image registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-ords:$ords_version exists, skip compile and reuse it..."
+  echo ">>> docker image registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-ords:$ords_version exists, pull from aliyun docker repository..."
+  docker pull registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-ords:$ords_version
 fi;
 
 ##############################################################################################################
