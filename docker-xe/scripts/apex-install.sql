58ac74ec3f51747983234edf547541fc03b062a1
wfg2513148 <wfgdlut@gmail.com>
2019-09-16 11:25:56 +0800

    test

--- docker-xe/scripts/apex-install.sql
+++ docker-xe/scripts/apex-install.sql
@@ -1,30 +1,5 @@
--- Example call:
--- $ORACLE_HOME/bin/sqlplus sys/oracle@localhost/XEPDB1 as sysdba @apex-install.sql Welc0me@1
---
--- Takes in 2 parameters:
--- 1: admin_email ex: wfgdlut@gmail.com
--- 2: admin_pass ex: Welc0me@1
-
 -- Must be run as SYS
 whenever sqlerror exit sql.sqlcode
-
--- Parameters: If using to create another user later on, just modify this section
-define admin_email = '&1'
-define admin_pass = '&2'
-
--- Validate parameters
-declare
-begin
-  if '&admin_email' is null then
-    raise_application_error(-20001, 'Param 1: admin_email missing');
-  end if;
-
-  if '&admin_pass' is null then
-    raise_application_error(-20001, 'Param 2: admin_pass missing');
-  end if;
-
-end;
-/
 
 -- Install APEX
 @apexins.sql SYSAUX SYSAUX TEMP /i/
