47e3a8b75b9a79af5d4518e0a969daaa2307c24e
wfg2513148 <wfgdlut@gmail.com>
2019-09-16 11:28:13 +0800

    ulimit oracle database account expire time

--- docker-xe/scripts/apex-install.sql
+++ docker-xe/scripts/apex-install.sql
@@ -1,6 +1,10 @@
 -- Must be run as SYS
 whenever sqlerror exit sql.sqlcode
 
+-- Unlimit account password expire time
+alter profile DEFAULT limit PASSWORD_REUSE_TIME unlimited;
+alter profile DEFAULT limit PASSWORD_LIFE_TIME  unlimited;
+
 -- Install APEX
 @apexins.sql SYSAUX SYSAUX TEMP /i/
 
