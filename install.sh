e563ba6c1223ac4a84aa37f40ceb906636a20baf
wfg2513148 <wfgdlut@gmail.com>
2019-09-16 13:59:40 +0800

    test

--- install.sh
+++ install.sh
@@ -187,7 +187,7 @@ cd $work_path/docker-ords/
 
 if [[ "$(docker images -q registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-xe:$db_version 2> /dev/null)" == "" ]]; then
   echo ">>> docker image registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-ords:$ords_version does not exist, begin to build docker image..."
-    docker build -t registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-ords:$ords_version .
+  docker build -t registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-ords:$ords_version .
 else
   echo ">>> docker image registry-vpc.cn-shanghai.aliyuncs.com/kwang/oracle-ords:$ords_version is found, skip compile step and go on..."
 fi;
